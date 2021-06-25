eb345bb3a30f9d98133330b34d2e3be9214f0269
Davyd <davydhyk@gmail.com>
2021-06-25 20:48:53 +0300

    Final Commit

---
+++ DAL/Migrations/20210625004425_Initial.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Destinations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Distance = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destinations", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProductTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ProcessingTime = table.Column<TimeSpan>(type: "time(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "VehicleTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleTypes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Width = table.Column<int>(type: "int", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    Length = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    ProductTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_ProductTypes_ProductTypeId",
+                        column: x => x.ProductTypeId,
+                        principalTable: "ProductTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProductTypeVehicleType",
+                columns: table => new
+                {
+                    ProductTypesId = table.Column<int>(type: "int", nullable: false),
+                    VehicleTypesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypeVehicleType", x => new { x.ProductTypesId, x.VehicleTypesId });
+                    table.ForeignKey(
+                        name: "FK_ProductTypeVehicleType_ProductTypes_ProductTypesId",
+                        column: x => x.ProductTypesId,
+                        principalTable: "ProductTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductTypeVehicleType_VehicleTypes_VehicleTypesId",
+                        column: x => x.VehicleTypesId,
+                        principalTable: "VehicleTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    MaxWidth = table.Column<int>(type: "int", nullable: false),
+                    MaxHeight = table.Column<int>(type: "int", nullable: false),
+                    MaxLength = table.Column<int>(type: "int", nullable: false),
+                    MaxWeight = table.Column<int>(type: "int", nullable: false),
+                    FreeDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    VehicleTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_VehicleTypes_VehicleTypeId",
+                        column: x => x.VehicleTypeId,
+                        principalTable: "VehicleTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StartDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DeliveryStartDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DeliveryEndDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    DestinationId = table.Column<int>(type: "int", nullable: false),
+                    VehicleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Destinations_DestinationId",
+                        column: x => x.DestinationId,
+                        principalTable: "Destinations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "Id", "Distance", "Name" },
+                values: new object[,]
+                {
+                    { 1, 20000, "New Orleans" },
+                    { 2, 122000, "Baton Rouge" },
+                    { 3, 196000, "Lafayette" },
+                    { 4, 70000, "Houma" },
+                    { 5, 54000, "Kansas City" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductTypes",
+                columns: new[] { "Id", "Name", "ProcessingTime" },
+                values: new object[,]
+                {
+                    { 1, "Machinery", new TimeSpan(0, 0, 30, 0, 0) },
+                    { 2, "Furniture", new TimeSpan(0, 0, 45, 0, 0) },
+                    { 3, "Animals", new TimeSpan(0, 1, 30, 0, 0) },
+                    { 4, "Medicine", new TimeSpan(0, 0, 15, 0, 0) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "VehicleTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Truck" },
+                    { 2, "Van" },
+                    { 3, "Car" },
+                    { 4, "F1 Car" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductTypeVehicleType",
+                columns: new[] { "ProductTypesId", "VehicleTypesId" },
+                values: new object[,]
+                {
+                    { 4, 4 },
+                    { 4, 3 },
+                    { 4, 2 },
+                    { 2, 2 },
+                    { 3, 4 },
+                    { 1, 1 },
+                    { 2, 1 },
+                    { 4, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Height", "Length", "Name", "Price", "ProductTypeId", "Weight", "Width" },
+                values: new object[,]
+                {
+                    { 4, 50, 20, "Feeding Station", 4499, 1, 4500, 100 },
+                    { 3, 24, 37, "Pills", 15, 4, 2000, 43 },
+                    { 1, 50, 50, "Jerboa", 500, 3, 5123, 50 },
+                    { 2, 50, 50, "Cage", 55, 2, 5000, 50 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Vehicles",
+                columns: new[] { "Id", "FreeDate", "MaxHeight", "MaxLength", "MaxWeight", "MaxWidth", "Name", "Speed", "VehicleTypeId" },
+                values: new object[,]
+                {
+                    { 3, new DateTime(2021, 6, 25, 3, 44, 24, 565, DateTimeKind.Local).AddTicks(2485), 30, 40, 4550, 45, "Ford Mustang", 215, 3 },
+                    { 1, new DateTime(2021, 6, 25, 3, 44, 24, 562, DateTimeKind.Local).AddTicks(9946), 50, 50, 5500, 50, "McLaren MCL34", 350, 4 },
+                    { 2, new DateTime(2021, 6, 25, 3, 44, 24, 565, DateTimeKind.Local).AddTicks(2455), 30, 40, 4654, 45, "Ferrari SF1000", 354, 4 },
+                    { 5, new DateTime(2021, 6, 25, 3, 44, 24, 565, DateTimeKind.Local).AddTicks(2494), 210, 450, 2025000, 180, "Mercedes-Benz Actros 6", 105, 1 },
+                    { 4, new DateTime(2021, 6, 25, 3, 44, 24, 565, DateTimeKind.Local).AddTicks(2490), 65, 21, 75000, 110, "Ford Transit", 135, 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_DestinationId",
+                table: "Orders",
+                column: "DestinationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ProductId",
+                table: "Orders",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_VehicleId",
+                table: "Orders",
+                column: "VehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTypeVehicleType_VehicleTypesId",
+                table: "ProductTypeVehicleType",
+                column: "VehicleTypesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_VehicleTypeId",
+                table: "Vehicles",
+                column: "VehicleTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "ProductTypeVehicleType");
+
+            migrationBuilder.DropTable(
+                name: "Destinations");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+
+            migrationBuilder.DropTable(
+                name: "ProductTypes");
+
+            migrationBuilder.DropTable(
+                name: "VehicleTypes");
+        }
+    }
+}
